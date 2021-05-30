ae96a626ae2150ef707bef206c6445418ca15b82
JingShen <jingshen.wei@temus.com>
2021-05-30 16:46:28 +0800

    update url

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-
 import {
   PRODUCT_CREATE,
   PRODUCT_UPDATE,
@@ -8,9 +7,15 @@ import {
   PRODUCT_FETCH_ALL,
 } from "../constants/productConstants";
 
+const API = axios.create({
+  baseURL: "https://tiger-grocery-inventory.herokuapp.com/",
+});
+
 export const createProduct = (form) => async (dispatch) => {
   try {
-    const { data } = await axios.post("/products", form);
+    // const { data } = await axios.post("/products", form);
+    const { data } = await API.post("/products", form);
+
     dispatch({ type: PRODUCT_CREATE, payload: data });
   } catch (error) {
     console.log(error);
@@ -19,7 +24,9 @@ export const createProduct = (form) => async (dispatch) => {
 
 export const fetchAllProducts = () => async (dispatch) => {
   try {
-    const { data } = await axios.get("/products");
+    // const { data } = await axios.get("/products");
+    const { data } = await API.get("/products");
+
     dispatch({ type: PRODUCT_FETCH_ALL, payload: data });
   } catch (error) {
     console.log(error);
@@ -28,7 +35,9 @@ export const fetchAllProducts = () => async (dispatch) => {
 
 export const deleteProduct = (id) => async (dispatch) => {
   try {
-    await axios.delete(`/products/${id}`);
+    // await axios.delete(`/products/${id}`);
+    await API.delete(`/products/${id}`);
+
     dispatch({ type: PRODUCT_DELETE, payload: id });
   } catch (error) {
     console.log(error);
@@ -37,7 +46,9 @@ export const deleteProduct = (id) => async (dispatch) => {
 
 export const deleteProducts = (idArr) => async (dispatch) => {
   try {
-    await axios.post(`/products/delete`, idArr);
+    // await axios.post(`/products/delete`, idArr);
+    await API.post(`/products/delete`, idArr);
+
     dispatch({ type: PRODUCT_DELETE_ALL, payload: idArr });
   } catch (error) {
     console.log(error);
@@ -46,7 +57,9 @@ export const deleteProducts = (idArr) => async (dispatch) => {
 
 export const updateProduct = (id, form) => async (dispatch) => {
   try {
-    const { data } = await axios.patch(`/products/${id}`, form);
+    // const { data } = await axios.patch(`/products/${id}`, form);
+    const { data } = await API.patch(`/products/${id}`, form);
+
     dispatch({ type: PRODUCT_UPDATE, payload: data });
   } catch (error) {
     console.log(error);
